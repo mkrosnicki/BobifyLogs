e53cb3d27512d486a52d59ad96b1e606766ee387
Maciej <maciejkrosnicki@gmail.com>
2022-01-12 20:48:34 +0100

    Initial commit

---
+++ src/main/java/com/mkrosnicki/bobifylogs/bobifylogs/BobifyLogsApplication.java
@@ -0,0 +1,13 @@
+package com.mkrosnicki.bobifylogs.bobifylogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobifyLogsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BobifyLogsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mkrosnicki/bobifylogs/bobifylogs/BobifyLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkrosnicki.bobifylogs.bobifylogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobifyLogsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
